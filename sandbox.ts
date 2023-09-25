79396f40da504c5e51d350ec2700ec4e0f87bad6
Nick Sheward <nickshwrd@gmail.com>
2023-09-25 11:36:27 +0100

    Added simple type check to circ function

--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,11 @@
 // tsc 'filename.ts' - w , auto compile every save
+
 // Can't change the type of a variable, even if declared with let
-var char = "yoshi";
-var age = 30;
-var circ = function (diameter) { return diameter * Math.PI; };
+
+let char = "yoshi";
+
+let age = 30;
+
+const circ = (diameter: number) => diameter * Math.PI;
+
 console.log(circ(7.5));
