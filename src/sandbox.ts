c8b7ecaa1e7bf52c27c733d95300e5c67d7f38be
Nick Sheward <nickshwrd@gmail.com>
2023-09-25 13:47:27 +0100

    Created tsconfig

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,55 +1,86 @@
 // tsc 'filename.ts' - w , auto compile every save
+// tsc --init to create tsconfig
+
 // Can't change the type of a variable, even if declared with let
+
 // Basics
 // let char = "yoshi";
+
 // let age = 30;
+
 // Type check in function
 // const circ = (diameter: number) => diameter * Math.PI;
+
 // console.log(circ(7.5));
+
 // Arrays
-var arr = ["hello", "goodbye", "night"];
+
+const arr = ["hello", "goodbye", "night"];
+
 arr.push("morning");
 // TypeScript only allows pushing of same type, err
 // arr.push(3);
-var arrMixed = [2, "hello", 4, "goodbye"];
+
+const arrMixed = [2, "hello", 4, "goodbye"];
+
 // Allows pushing of any types already present in array
 arrMixed.push("night");
+
 // Objects
 // Can't later change types of properties
-var obj = {
-    name: "barry",
-    age: 500,
-    favColour: "green",
+let obj = {
+  name: "barry",
+  age: 500,
+  favColour: "green",
 };
+
 // You can reassign values to each property, but the object structure must stay the same
 // I.e. can't remove or add new properties
 obj = {
-    name: "frank",
-    age: 300,
-    favColour: "pink",
+  name: "frank",
+  age: 300,
+  favColour: "pink",
 };
+
 // Explicit types - variables
-var username;
-var age;
-var isLoggedIn;
+let username: string;
+let age: number;
+let isLoggedIn: boolean;
+
 username = "barrmoonshine";
 age = 1000000;
 isLoggedIn = true;
+
 // Explicit types, arrays
-var posts = []; // posts an array of strings, initialise with empty value
+let posts: string[] = []; // posts an array of strings, initialise with empty value
+
 posts = ["hello world", "my first post"];
 posts.push("cool post"); // Must initialise with value of empty array before mutating
+
 console.log(posts);
+
 // Union types, what types are allowed, example array
-var mixed = [];
+let mixed: (string | number | boolean)[] = [];
+
 mixed.push("hello");
 mixed.push(100);
+
 console.log(mixed);
+
 // Example string with union types
-var uid;
+let uid: string | number;
 uid = 123;
 uid = "123";
+
 // Objects
-var myObj; // Note array is a type of object
+let myObj: object; // Note array is a type of object
 myObj = { message: "hello", likes: 50, published: true };
-var myObjTwo; // Stricter object setting, array would not be allowed
+
+let myObjTwo: {
+  message: string;
+  likes: number;
+  published: boolean;
+}; // Stricter object setting, array would not be allowed
+
+// Any type, can overwrite the any type
+let recipes: any;
