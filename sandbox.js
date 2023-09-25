8ae77e34b04351302d5191407eef946d696ad741
Nick Sheward <nickshwrd@gmail.com>
2023-09-25 11:49:13 +0100

    Arrays and objects notes added

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,30 @@
 // tsc 'filename.ts' - w , auto compile every save
 // Can't change the type of a variable, even if declared with let
-var char = "yoshi";
-var age = 30;
-var circ = function (diameter) { return diameter * Math.PI; };
-console.log(circ(7.5));
+// Basics
+// let char = "yoshi";
+// let age = 30;
+// Type check in function
+// const circ = (diameter: number) => diameter * Math.PI;
+// console.log(circ(7.5));
+// Arrays
+var arr = ["hello", "goodbye", "night"];
+arr.push("morning");
+// TypeScript only allows pushing of same type, err
+// arr.push(3);
+var arrMixed = [2, "hello", 4, "goodbye"];
+// Allows pushing of any types already present in array
+arrMixed.push("night");
+// Objects
+// Can't later change types of properties
+var obj = {
+    name: "barry",
+    age: 500,
+    favColour: "green",
+};
+// You can reassign values to each property, but the object structure must stay the same
+// I.e. can't remove or add new properties
+obj = {
+    name: "frank",
+    age: 300,
+    favColour: "pink",
+};
--- sandbox.ts
+++ sandbox.ts
@@ -2,10 +2,41 @@
 
 // Can't change the type of a variable, even if declared with let
 
-let char = "yoshi";
+// Basics
+// let char = "yoshi";
 
-let age = 30;
+// let age = 30;
 
-const circ = (diameter: number) => diameter * Math.PI;
+// Type check in function
+// const circ = (diameter: number) => diameter * Math.PI;
 
-console.log(circ(7.5));
+// console.log(circ(7.5));
+
+// Arrays
+
+const arr = ["hello", "goodbye", "night"];
+
+arr.push("morning");
+// TypeScript only allows pushing of same type, err
+// arr.push(3);
+
+const arrMixed = [2, "hello", 4, "goodbye"];
+
+// Allows pushing of any types already present in array
+arrMixed.push("night");
+
+// Objects
+// Can't later change types of properties
+let obj = {
+  name: "barry",
+  age: 500,
+  favColour: "green",
+};
+
+// You can reassign values to each property, but the object structure must stay the same
+// I.e. can't remove or add new properties
+obj = {
+  name: "frank",
+  age: 300,
+  favColour: "pink",
+};
